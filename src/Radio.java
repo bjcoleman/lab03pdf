0a0ee3b307ca93c6f9db92b66a7deb200fd14eec
Ben Coleman <colemanb@moravian.edu>
2020-02-05 09:42:48 -0500

    Initial commit

---
+++ src/Radio.java
@@ -0,0 +1,8 @@
+/*
+ * CSCI121 - Computer Science II - Spring 2019
+ * Instructors: Thyago Mota & Jeffrey Bush
+ * Description: Lab 03 - Radio
+ * Your name(s):
+ */
+
+// TODO: implement the Radio class according to specifications
---
+++ src/RadioDriver.java
@@ -0,0 +1,53 @@
+/*
+ * CSCI121 - Computer Science II - Spring 2019
+ * Instructors: Thyago Mota & Jeffrey Bush
+ * Description: Lab 03 - RadioDriver
+ * Your name(s):
+ */
+
+public class RadioDriver {
+
+    public static void main(String[] args) {
+
+        /* TODO: declare a radio variable named r1 and use it to refer to a radio object created using the parameter-less constructor; then print the radio information */
+
+
+
+
+        /* TODO: now use r1 to turn the radio ON; then switch the radio to the FM band */
+
+
+
+
+        /* TODO: use a loop to increase the volume up to 40db; print the radio information when you are done */
+
+
+
+
+        /* TODO: now tune the radio object to 104.5MHz using a for loop; do the math first to figure it out how many times you should call tune forward using r1; after the loop, print the radio info again and verify if you are tuned to the FM station at 104.5MHz. */
+
+
+
+
+        /* TODO: switch the radio object to the AM band and print the radio settings again */
+
+
+
+
+        /* TODO: use a loop to reduce the volume to 30dbs; print the radio object again */
+
+
+
+
+        /* TODO: now tune the radio to 994KHz using a for loop; again, do the math first to figure it out how many times you should tune backwards using r1; after the loop, print the radio information again and verify if you are tuned to AM station at 994KHz. */
+
+
+
+
+        /* TODO: turn the radio OFF and try to print it one more time */
+
+
+
+
+    }
+}
